929e7225563d42aaf247af26a2b2474c97ccf461
celiszin <teste153ovo@gmail.com>
2025-02-18 23:21:54 -0300

    1

---
+++ src/screens/TaskList.js
@@ -0,0 +1,19 @@
+import { Text, View, StyleSheet } from "react-native-web"
+
+export default function TaskList(){
+    return(
+        <View style={styles.container}>
+            <Text>
+                TaskList
+            </Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    }
+})
