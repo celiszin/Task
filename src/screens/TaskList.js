821391b97d78b68f60f5fcd33f70feaccc34252f
Luiz <teste153ovo@gmail.com>
2025-02-20 22:35:45 -0300

    2

--- src/screens/TaskList.js
+++ src/screens/TaskList.js
@@ -1,11 +1,28 @@
-import { Text, View, StyleSheet } from "react-native-web"
+import { Text, View, StyleSheet, ImageBackground } from "react-native-web"
+
+import moment from "moment"
+import '/moment/locale/pt-br'
+
+import todayImage from '../../assets/today.jpg'
 
 export default function TaskList(){
+    const today = moment().local('pt-br').format('ddd, D [de] MMMM')
+
     return(
         <View style={styles.container}>
-            <Text>
-                TaskList
-            </Text>
+
+            <View style={styles.titleBar}></View>
+                <ImageBackground source={todayImage} style={styles.background}>
+                    <View style={styles.titleBar}>
+                        <Text style={styles.title}>Hoje</Text>
+                        <Text style={styles.subtitle}>{today}</Text>
+                    </View>
+                </ImageBackground>
+
+                <View style ={styles.taskList}>
+
+                </View>
+
         </View>
     )
 }
@@ -13,7 +30,29 @@ export default function TaskList(){
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center'
+        
+    },
+    background: {
+        flex: 3
+    },
+    taskList:{
+        flex:7
+    },
+    titleBar:{
+        flex: 1,
+        justifyContent: "flex-end",
+        backgroundColor: 'black'
+    },
+    title:{
+        color: 'white',
+        fontSize: 50,
+        marginLeft: 20,
+        marginBottom: 20
+    },
+    subtitle:{
+        color: 'white',
+        fontSize: 20,
+        marginLeft: 20,
+        marginBottom: 30
     }
 })
